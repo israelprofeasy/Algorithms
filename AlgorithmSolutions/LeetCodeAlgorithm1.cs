f768de9c95d0cdca04125c92d2e57348ef859c3c
israelprofeasy <israeloluwafemi44@gmail.com>
2023-06-29 13:48:31 +0100

    two sum

--- AlgorithmSolutions/LeetCodeAlgorithm1.cs
+++ AlgorithmSolutions/LeetCodeAlgorithm1.cs
@@ -228,6 +228,24 @@ namespace AlgorithmSolutions
 
 
         }
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            var left = 0;
+            var right = numbers.Length - 1;
+            while (left < right)
+            {
+                if (numbers[left] + numbers[right] == target)
+                {
+                    return new int[] { left + 1, right + 1 };
+                }
+                else if (numbers[left] + numbers[right] > target)
+                    right--;
+                else
+                    left++;
+
+            }
+            return new int[] { };
+        }
     }
 }
 
