0e3e14cb70e1b816f535426f23e2217fe7d3ee87
israelprofeasy <israeloluwafemi44@gmail.com>
2023-08-19 04:02:11 +0100

    turing trials

---
+++ AlgorithmSolutions/Turing.cs
@@ -0,0 +1,104 @@
+﻿using System;
+namespace AlgorithmSolutions
+{
+    public class Turing
+    {
+        public static void Main()
+        {
+            var ss = "({})";
+            Console.WriteLine(IsValid(ss));
+            var dd = new string[] { "5", "2", "C", "D", "+", "+" };
+            //output = 50
+            Console.WriteLine(Score(dd));
+            var cd = new int[][] { new int[] { 5, 7, 3, 9, 4, 9, 8, 3, 1 }, new int[] { 1, 2, 2, 4, 4, 1 }, new int[] { 1, 2, 3 } };
+            //output = 8
+            var cd2 = new int[][] { new int[] { 5, 7, 3 }, new int[] { 1, 2, 2 }, new int[] { 1, 2, 3 } };
+            //output = -1
+            Console.WriteLine(Card(cd2));
+        }
+        static bool IsValid(string str)
+        {
+            var op = new List<char> { '(', '{', '[' };
+            var cl = new List<char> { ')', '}', ']' };
+            var stk = new Stack<char>();
+            if (cl.Contains(str[0]))
+                return false;
+            foreach (var c in str)
+            {
+                if (op.Contains(c))
+                {
+                    stk.Push(c);
+                }
+                else if (stk.Count == 0)
+                    return false;
+                else
+                {
+                    var index = cl.IndexOf(c);
+                    if (op[index] == stk.Peek())
+                        stk.Pop();
+                    else
+                    {
+                        return false;
+                    }
+
+                }
+
+            }
+            return stk.Count == 0;
+        }
+
+        static int Score(string[] cha)
+        {
+            var score = new Stack<int>();
+            var result = 0;
+            int lst = 0;
+            foreach (var c in cha)
+            {
+                int n;
+                if (int.TryParse(c, out n))
+                {
+                    score.TryPeek(out lst);
+                    score.Push(n);
+                }
+                if (c == "C")
+                {
+                    score.Pop();
+                }
+                if (c == "D")
+                {
+                    var cc = score.Peek();
+                    lst = cc;
+                    score.Push(2 * cc);
+                }
+                if (c == "+")
+                {
+                    var cc = score.Peek();
+                    score.Push(lst + cc);
+                }
+            }
+
+            while (score.Count > 0)
+            {
+                //Console.WriteLine(score.Peek());
+                result += score.Pop();
+            }
+
+            return result;
+        }
+
+        static int Card(int[][] cards)
+        {
+            var ls = cards.ToList();
+            Console.WriteLine("====================");
+            ls.OrderBy(x => x.Count());
+            Console.WriteLine("====================");
+            var lowCount = ls[1];
+            Console.WriteLine($"===================={lowCount.Count()} ");
+            var highCount = ls[0];
+            Console.WriteLine($"===================={highCount.Count()}");
+
+            return highCount.Count() - lowCount.Count() == 0 ? -1 : highCount[lowCount.Count()];
+        }
+    }
+}
+
